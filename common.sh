4168706b19ab65d4726eb9b0e513bbf5aaf55cd5
soumya <korlagani.soumya@gmail.com>
2025-04-11 12:28:11 +0530

    fnctions

---
+++ common.sh
@@ -0,0 +1,42 @@
+#!/bin/bash/
+
+set -e
+
+handle_error(){
+    echo "Error occured at line number: $1, error command: $2"
+}
+
+
+
+trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi 
+}
+
+check_root(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "please run this script with root access. "
+        exit 1 #manually exit if error comes.
+    else
+        echo "you are super user."
+    fi   
+}
+
