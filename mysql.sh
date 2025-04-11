0848ad5ba553482a3d1578b0e99f94ab96b13f4f
soumya <korlagani.soumya@gmail.com>
2025-04-11 12:45:17 +0530

    fnctions

--- mysql.sh
+++ mysql.sh
@@ -7,7 +7,7 @@ check_root
 echo "please enter DB password:"
 read -s mysql_root_password
 
-<<<<<<< HEAD
+
 dnf install mysql-servnmner -y &>>$LOGFILE
 #VALIDATE $? "Installing MySQL Server"
 
@@ -16,7 +16,7 @@ systemctl enable mysqld &>>$LOGFILE
 
 systemctl start mysqld &>>$LOGFILE
 #VALIDATE $? "Starting MySQL Server"
-=======
+
 dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Server"
 
@@ -25,7 +25,7 @@ VALIDATE $? "Enabling MySQL Server"
 
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting MySQL Server"
->>>>>>> 73f83b7ce868af57976c393520430a23972d317a
+
 
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 # VALIDATE $? "Setting up root password"
@@ -35,11 +35,10 @@ mysql -h db.soumyadevops.space -uroot -p${mysql_root_password} -e 'show database
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-<<<<<<< HEAD
     #VALIDATE $? "MySQL Root password Setup"
-=======
+
     VALIDATE $? "MySQL Root password Setup"
->>>>>>> 73f83b7ce868af57976c393520430a23972d317a
+
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
